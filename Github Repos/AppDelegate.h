a3966253493f7b576e103150a6bf0e80f019f4d2
Rob Wymer <wymer1120@gmail.com>
2017-07-17 13:28:32 -0400

    First Commit

---
+++ Github Repos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Github Repos
+//
+//  Created by Alex Wymer  on 2017-07-17.
+//  Copyright © 2017 Sav Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
